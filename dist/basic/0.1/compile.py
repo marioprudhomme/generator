67c81470c27508bf1d884e0c729e197dbdd378a5
mario-digitalstate <mario@digitalstate.ca>
2020-04-18 14:08:42 -0400

    No message

--- dist/basic/0.1/compile.py
+++ dist/basic/0.1/compile.py
@@ -1,3 +1,7 @@
+import os
+from distutils.dir_util import copy_tree
+import yaml
+
 def main(src: str):
     #
     # Compile src into dist
@@ -6,8 +10,19 @@ def main(src: str):
     #    src (str): The src directory.
     #
 
-    print ('Compiling...')
-    print ('Done.')
+    print('Compiling...')
+    dist = os.getcwd()
+
+    print('Moving dist to .compiled directory...')
+    copy_tree(dist + '/src/', src + '/.compiled/')
+
+    print('Copying src files to .compiled directory...')
+    copy_tree(src + '/.compiled/', src + '/src/')
+
+    print('Replacing placeholders...')
+    app = yaml.load(src + 'config/app.yaml', Loader=yaml.FullLoader)
+
+    print('Done.')
 
 
 if __name__ == '__main__':
